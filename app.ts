5a4318da69fff53e08955c3bb6cf3f6216854cef
andrea-junia <andre3aj.f@gmail.com>
2022-07-17 16:34:13 -0300

    commit iniciaol dio - introducao ao typescript

---
+++ app.ts
@@ -0,0 +1,7 @@
+//assuntos tratados 
+
+//validacao de tipos de dados com typescript
+//number, string, any, unknown, void, callback, never, throw 
+//objeto, inteface e enum
+
+console.log('DIO -> aula pratica typescript')
